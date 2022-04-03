79c73ed69e5358391c15b382b90009adfbd4d045
Malvin-mi <michaelogunyemi3@gmail.com>
2022-04-03 03:16:58 +0100

    new dawn

---
+++ Kodecamp2.0/giftcarder/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+         <h1>Giftcarder</h1>
+         <h3>Get your giftcards here.</h3>
+    </div>
+    
+  )
+}
+
+export default Header
